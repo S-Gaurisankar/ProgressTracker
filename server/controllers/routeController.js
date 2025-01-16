4fb6847460a2174fcd442eebd409550f1691375b
S. Gaurisankar <gaurisankar2003@gmail.com>
2025-01-16 14:17:10 +0530

    ADding logs route

--- server/controllers/routeController.js
+++ server/controllers/routeController.js
@@ -7,6 +7,7 @@ const getTask = require('../routes/getTask');
 const addTask = require('../routes/addTask');
 const updateTaskByJiraTicket = require('../routes/updateTask');
 const queryTask = require('../routes/queryTask');
+const log = require('../routes/logs');
 const deleteTaskByJiraTicket = require('../routes/deleteTask');
 
 app.use(express.json());
@@ -16,6 +17,7 @@ router.get('/', getAllTasks);   //Gets all tasks
 router.get("/query",queryTask); //Get a task with any params (jira_ticket, status, department, etc.)
 router.get("/:id", getTask);  //Get a task by id (MongoDB ID)
 
+router.get("/logs", log);
 router.post("/post", addTask);  //Add a new task
 
 router.put("/update/:jira_ticket", updateTaskByJiraTicket);  //Edit the task by jira_ticket
@@ -23,3 +25,4 @@ router.put("/update/:jira_ticket", updateTaskByJiraTicket);  //Edit the task by 
 router.delete("/delete/:jira_ticket", deleteTaskByJiraTicket);  //Delete a task by jira_ticket
 
 module.exports = router;
+
